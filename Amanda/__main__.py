7602c700badcfbe39b999087a3c593c7c22165ac
akshay695 <102814990+akshay695@users.noreply.github.com>
2022-04-10 05:54:53 +0530

    Update __main__.py

--- Amanda/__main__.py
+++ Amanda/__main__.py
@@ -63,7 +63,7 @@ from Amanda.modules.helper_funcs.misc import paginate_modules
 from Amanda.modules.helper_funcs.readable_time import get_readable_time
 
 PM_START_TEXT = """
-Hello there, I'm 𝓐𝓶𝓪𝓷𝓭𝓪
+Hello there, I'm Filter bot
 I'm a Powerful group manager bot With Cool Modules. Made by [𝕿𝖍𝖆𝖗𝖚𝖐 𝕽𝖊𝖓𝖚𝖏𝖆• 🇱🇰](https://t.me/TharukRenuja)
 Hit /help to find my list of available commands
  
@@ -72,32 +72,32 @@ Hit /help to find my list of available commands
 buttons = [
     [
         InlineKeyboardButton(
-            text="📢Updates Channel", url="https://t.me/SLBotsofficial"
+            text="📢Updates Channel", url="https://telegra.ph/Update-and-Support-04-10"
         ),
         InlineKeyboardButton(
-            text="👥 Support Group", url="https://t.me/trtechguide"
-        ),
-    ],
-    [
-        InlineKeyboardButton(
-            text="📜Source", url="https://github.com/TR-TECH-GUIDE/Amanda"
-        ),
-        InlineKeyboardButton(
-            text="❔ Help", url="http://t.me/TheAmandabot?start=help"
+            text="👥 Support Group", url="https://telegra.ph/Update-and-Support-04-10"
         ),
     ],
     [
         InlineKeyboardButton(
-            text="➕ Add Amanda to your group ➕",
-            url="t.me/TheAmandabot?startgroup=true",
+            text="Bot Update", url="https://telegra.ph/Update-and-Support-04-10"
+        ),
+        InlineKeyboardButton(
+            text="❔ Help", url="http://t.me/BetterFilterXBot?start=help"
+        ),
+    ],
+    [
+        InlineKeyboardButton(
+            text="➕ Add Bot to your group ➕",
+            url="t.me/BetterFilterXBot@BetterFilterXBot?startgroup=true",
         ),
     ],
 ]
 
-Amanda_IMG = "https://telegra.ph/file/04d73369440abc48ab3ce.png"
+Amanda_IMG = "https://te.legra.ph/file/aaed3fc37902acc3f9400.jpg"
 
 HELP_STRINGS = f"""
-*Main Commands :* [👩](https://telegra.ph/file/04d73369440abc48ab3ce.png)
+*Main Commands :* [👩](https://te.legra.ph/file/aaed3fc37902acc3f9400.jpg)
 ✪ /start: Starts me! You've probably already used this.
 ✪ /help: Click this, I'll let you know about myself!
 ✪ /donate: You can support my creater using this command.
@@ -374,7 +374,7 @@ def Amanda_about_callback(update, context):
     if query.data == "aboutmanu_":
         query.message.edit_text(
             text=f"* Hi There  The name's {dispatcher.bot.first_name} \n\nAs  You I'm a next generational group management bot developed by Amanda Updates.* "
-            f"\n\n Join [SLBotsofficial](https://t.me/SLBotsofficial) To Keep Yourself Updated About {dispatcher.bot.first_name}"
+            f"\n\n Join [SLBotsofficial](https://te.legra.ph/file/aaed3fc37902acc3f9400.jpg) To Keep Yourself Updated About {dispatcher.bot.first_name}"
             f"\n\n I have the normal GROUP MANAGING functions like flood control, a warning system etc but I mainly have the advanced and handy Antispam system and the SIBYL banning system which safegaurds and helps your group from spammers."
             f"\n\nI Can Manage Your Groups Smoothly, With Some Special Features"
             f"\n\nYou Can Know More About Me By Clicking The Below Buttons",
@@ -426,7 +426,7 @@ def Amanda_about_callback(update, context):
     elif query.data == "aboutmanu_credit":
         query.message.edit_text(
             text=f"*{dispatcher.bot.first_name} Is the redisigned version of Raya for the best performance.*"
-            f"\n\nBased on [Amanda's Brother](https://t.me/trtechguidebot)."
+            f"\n\nBased on [Amanda's Brother](https://te.legra.ph/file/aaed3fc37902acc3f9400.jpg)."
             f"\n\n{dispatcher.bot.first_name}'s source code was written by Tharuk Renuja"
             f"\n\nIf Any Question About {dispatcher.bot.first_name}, \nLet Us Know At @{SUPPORT_CHAT}.",
             parse_mode=ParseMode.MARKDOWN,
